970e3ec879a4724893689f60c85228b128f5174c
Tim-Mclennan <timmymclennan@gmail.com>
2022-11-24 19:31:07 +1100

    finalised changes to the 'Settings' section

--- README.md
+++ README.md
@@ -4,3 +4,8 @@ Hi there!
 
 The objective of creating this Iphone clone is to showcase the skills I have learnt while attending Nology's software engineering program. 
 
+
+For this particular project, I have decided to use the BEM notation to structure my properties. BEM stands for 'Block__Element--Modifier'. Using BEM is an efficient way to change and edit CSS properties at a large scale.
+
+
+I have also included the use of external pre-set SCSS variables which contain properties. Using external SCSS files for preset variables is a great tool to use for larger, complex projects as it makes CSS more modular and easier to read. These external properties are located within a seperate directory (scss-docs) and will be imported with the '@import' command.
--- app-script.js
+++ app-script.js
@@ -1,29 +1,25 @@
 const home = document.getElementById("home");
 
 // const safari = document.getElementById("safari");
-const app = document.getElementsByClassName("iphone__screen--app");
+const apps = document.getElementsByClassName("iphone__screen__app");
 const cards = document.getElementsByClassName("iphone__screen__icons--card");
 
 
 
 /* SAFARI */
     cards[0].addEventListener("click", () => {
-        console.log("this works");
-         app[0].classList.add("open");
+         apps[0].classList.add("open");
 });
 home.addEventListener("click", (i) => {
-    console.log("home works");
-    app[0].classList.remove("open");
+    apps[0].classList.remove("open");
 
 });
 /* CAMERA */
 cards[1].addEventListener("click", () => {
-    console.log("this works");
-     app[1].classList.add("open");
+     apps[1].classList.add("open");
 });
 home.addEventListener("click", (i) => {
-    console.log("home works");
-    app[1].classList.remove("open");
+    apps[1].classList.remove("open");
 })
 
 
